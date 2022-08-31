740c2cf2e0f8c735af59a61c10e775ec677be621
Christian-RH <fenixcr.123@gmail.com>
2022-08-31 16:16:49 -0500

    include video capture and detection plate

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 from PyQt5.QtSerialPort import QSerialPort, QSerialPortInfo
-from PyQt5.QtCore import QIODevice, QPoint, QPropertyAnimation, QEasingCurve
+from PyQt5.QtCore import *
 from gui_design import *
+from PyQt5.QtGui import *
 import pyqtgraph as pg
 import numpy as np
+import cv2
+import imutils
+
+face_cascade = cv2.CascadeClassifier('models/haarcascade_plate_number.xml')
 
 class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
@@ -38,7 +43,7 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         # Movimiento de la ventana
         self.frame_superior.mouseMoveEvent = self.mover_ventana
 
-        #self.start_video()
+        self.start_video()
 
     def mover_menu(self):
         if True:
@@ -60,7 +65,7 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
         self.close()
         #cap.release()
         self.label.clear()
-        #pipeline.stop()
+        self.Work.stop()
 
     def control_btn_normal(self):
         self.showNormal()
@@ -93,6 +98,41 @@ class MyApp(QtWidgets.QMainWindow, Ui_MainWindow):
             self.showNormal()
             self.btn_normal.hide()
             self.btn_max.show()
+
+    def start_video(self):
+        self.Work = Work()
+        self.Work.start()
+        self.Work.Imageupd.connect(self.Imageupd_slot)
+
+    def Imageupd_slot(self, Image):
+        self.label_video.setPixmap(QPixmap.fromImage(Image))
+
+
+class Work(QThread):
+    Imageupd = pyqtSignal(QImage)
+
+    def run(self):
+        self.hilo_corriendo = True
+        self.cap = cv2.VideoCapture(0)
+        while self.hilo_corriendo:
+            ret, frame = self.cap.read()
+            if ret:
+                Image = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+                faces = face_cascade.detectMultiScale(Image, 1.1, 4)
+                for (x, y, w, h) in faces:
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (236,111,84), 2)
+
+                Image = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+                flip = cv2.flip(Image, 1)
+                frameu = imutils.resize(flip, width=640, height=480)
+                pic = QImage(frameu.data, frameu.shape[1], frameu.shape[0], QImage.Format_RGB888)
+                #pic = convertir_QT.scaled(320, 240, Qt.KeepAspectRatio)
+                self.Imageupd.emit(pic)
+
+    def stop(self):
+        self.hilo_corriendo = False
+        self.quit()
+
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication([])
